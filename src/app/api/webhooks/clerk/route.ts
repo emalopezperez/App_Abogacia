7fde958942e91600b790a9589e43908b7f8cd7f3
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-12-03 17:49:14 -0300

    Add synchronization with Clerk and MongoDB for user creation4

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,9 +1,15 @@
+/* eslint-disable camelcase */
+import { createUser } from "@/app/actions/user.actions";
+import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { Webhook } from "svix";
-import { createUser, deleteUser } from "@/app/actions/user.actions";
+import console from "console";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { Webhook } from "svix";
+
 
 export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -12,36 +18,47 @@ export async function POST(req: Request) {
     );
   }
 
-  const headerPayload = await req.headers;
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = (await headerPayload).get("svix-id");
+  const svix_timestamp = (await headerPayload).get("svix-timestamp");
+  const svix_signature = (await headerPayload).get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", { status: 400 });
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
   }
 
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent; // Definimos evt como WebhookEvent
+  let evt: WebhookEvent;
 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    }) as WebhookEvent; // Afirmación de tipo
+    }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return new Response("Error occured", { status: 400 });
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
 
+  // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
 
+  // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
@@ -56,19 +73,45 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
+    // Set public metadata
     if (newUser) {
-      // Set metadata (using your user management logic)
-      // You can call your DB logic here
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
     }
 
     return NextResponse.json({ message: "OK", user: newUser });
   }
 
-  if (eventType === "user.deleted") {
-    const { id } = evt.data;
-    const deletedUser = await deleteUser(id!);
-    return NextResponse.json({ message: "OK", user: deletedUser });
-  }
+  // // UPDATE
+  // if (eventType === "user.updated") {
+  //   const { id, image_url, first_name, last_name, username } = evt.data;
+
+  //   const user = {
+  //     firstName: first_name,
+  //     lastName: last_name,
+  //     username: username!,
+  //     photo: image_url,
+  //   };
+
+  //   const updatedUser = await updateUser(id, user);
+
+  //   return NextResponse.json({ message: "OK", user: updatedUser });
+  // }
+
+  // // DELETE
+  // if (eventType === "user.deleted") {
+  //   const { id } = evt.data;
+
+  //   const deletedUser = await deleteUser(id!);
+
+  //   return NextResponse.json({ message: "OK", user: deletedUser });
+  // }
+
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
 
   return new Response("", { status: 200 });
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-const isAdminRoute = createRouteMatcher(["/dashboard(.*)"]);
-const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/clerk"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isPublicRoute(req)) {
-    return NextResponse.next();
-  }
-
-  if (isAdminRoute(req)) {
-    const session = await auth();
-    if (session?.sessionClaims?.metadata?.role !== "admin") {
-      const url = new URL("/", req.url);
-      return NextResponse.redirect(url);
-    }
-  }
-
-  return NextResponse.next();
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk']
 });
-
+ 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
