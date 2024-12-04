8f78a442221c1a766fdd56f612aa0b0205ba7bf0
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-12-03 21:38:56 -0300

    Error en el Middelware de Clerk solucionadoF

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/","/sign-in","/sign-up", "/api/webhooks/clerk"]);
-
 const isAdminRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-const isMemberRoute = createRouteMatcher(["/client(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/clerk"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, sessionClaims } = await auth();
-
   if (isPublicRoute(req)) {
     return NextResponse.next();
   }
 
   if (isAdminRoute(req)) {
-    if (sessionClaims?.metadata?.role !== "admin") {
+    const session = await auth();
+    if (session?.sessionClaims?.metadata?.role !== "admin") {
       const url = new URL("/", req.url);
       return NextResponse.redirect(url);
     }
-  }
-
-  // Validar rutas de miembro autenticado
-  // if (isMemberRoute(req)) {
-  //   if (!userId || sessionClaims?.metadata?.role !== "member") {
-  //     const url = new URL("/", req.url);
-  //     return NextResponse.redirect(url);
-  //   }
-  // }
-
-  if (!userId) {
-    const url = new URL("/", req.url);
-    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
